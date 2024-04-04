e51912f7e1c2a767dae00d3a70eb4b51e799cbb7
Kevin MAS <maskevin.w@gmail.com>
2024-04-04 15:51:28 +0200

    fix: removing useless import

--- src/use-case/game/game-use-case.module.ts
+++ src/use-case/game/game-use-case.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { GameController } from 'src/controller/game/game.controller';
 import { GameDataService } from './game-data-service.service';
 import { GameUseCases } from './game.use-case';
@@ -7,7 +6,6 @@ import { GameUseCases } from './game.use-case';
 @Module({
 	controllers: [GameController],
 	exports: [GameUseCases],
-	imports: [ConfigService],
 	providers: [GameDataService, GameUseCases],
 })
 export class GameUseCasesModule {}
