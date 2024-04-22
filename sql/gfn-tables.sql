0f7f8be09a43e31f9557506ee7db4619441400d0
Kevin MAS <maskevin.w@gmail.com>
2024-04-22 06:42:14 +0200

    refactor: adding not null constraint to the sql generation for gfn tables

--- sql/gfn-tables.sql
+++ sql/gfn-tables.sql
@@ -5,8 +5,8 @@ ALTER SCHEMA public OWNER TO postgres;
 CREATE TABLE
     IF NOT EXISTS public.game (
         id SERIAL PRIMARY KEY,
-        title VARCHAR(255),
-        sortName VARCHAR(255)
+        title VARCHAR(255) NOT NULL,
+        sortName VARCHAR(255) NOT NULL
     );
 
 ALTER TABLE public.game OWNER TO postgres;
@@ -14,9 +14,9 @@ ALTER TABLE public.game OWNER TO postgres;
 CREATE TABLE
     IF NOT EXISTS public.variants (
         id SERIAL PRIMARY KEY,
-        appStore VARCHAR(255),
-        publisherName VARCHAR(255),
-        gameId INT,
+        appStore VARCHAR(255) NOT NULL,
+        publisherName VARCHAR(255) NOT NULL,
+        gameId INT NOT NULL,
         FOREIGN KEY (gameId) REFERENCES game (id)
     );
 
